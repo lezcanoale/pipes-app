3aa3d19d7cd1550188755ef47331d815914fa292
Alejandra Lezcano <ma.alexa2000@fpuna.edu.py>
2026-01-09 13:52:51 -0300

    feat: add routing and components for basic, number, uncommon, and custom pages
    - Implemented routing for basic, numbers, uncommon, and custom pages in app.routes.ts
    - Created Navbar component to display navigation links based on routes
    - Added basic, number, uncommon, and custom page components with placeholder content
    - Updated index.html to set the theme to Dracula
    - Integrated Tailwind CSS and DaisyUI for styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'basic',
+    title: 'Pipes Básicos',
+    loadComponent: () => import('../app/pages/basic-page/basic-page'),
+  },
+  {
+    path: 'numbers',
+    title: 'Numbers Pipe',
+    loadComponent: () => import('../app/pages/number-page/number-page'),
+  },
+  {
+    path: 'uncommon',
+    title: 'Pipes no tan comunes',
+    loadComponent: () => import('../app/pages/uncommon-page/uncommon-page'),
+  },
+  {
+    path: 'custom',
+    title: 'Pipes Personalizados',
+    loadComponent: () => import('../app/pages/custom-page/custom-page'),
+  },
+  {
+    path: '**',
+    redirectTo: 'basic',
+  },
+];
