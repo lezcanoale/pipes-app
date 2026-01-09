65391e5292267504b2a56b30c0d4215245dedfab
Alejandra Lezcano <ma.alexa2000@fpuna.edu.py>
2026-01-09 16:40:23 -0300

    feat: add date pipe functionality and update basic page layout

--- src/app/pages/basic-page/basic-page.ts
+++ src/app/pages/basic-page/basic-page.ts
@@ -1,10 +1,10 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, signal } from '@angular/core';
 import { TitleHeaderComponent } from '../../components/title-header/title-header';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, TitleHeaderComponent],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, TitleHeaderComponent, DatePipe],
   templateUrl: './basic-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -12,4 +12,16 @@ export default class BasicPageComponent {
   nameLower = signal('ALEJANDRA');
   nameUpper = signal('alejandra');
   fullName = signal('AleJaNdRa LeZcAnO');
+
+  customDate = signal(new Date());
+
+  thickingDate = effect((onCleanUp) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+    }, 1000);
+
+    onCleanUp(() => {
+      clearInterval(interval);
+    });
+  });
 }
