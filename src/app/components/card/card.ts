3c8ecceff5699b689a54b85d761ad8bcd59638c6
Alejandra Lezcano <ma.alexa2000@fpuna.edu.py>
2026-01-12 10:22:41 -0300

    feat: we use I18nSelectPipe and I18nPluralPipe to change how it looks a text when changing the gender and the amount of clients

--- src/app/components/card/card.ts
+++ src/app/components/card/card.ts
@@ -1,9 +1,11 @@
 import { I18nSelectPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 //Pipe para renderizar contenido que pasa del padre al hijo
 @Component({
   selector: 'app-card',
   imports: [I18nSelectPipe],
   templateUrl: './card.html',
 })
-export class Card {}
+export class Card {
+  title = input.required<string>();
+}
--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { TitleHeaderComponent } from '../../components/title-header/title-header';
 import { Card } from '../../components/card/card';
+import { I18nPluralPipe, I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Alejandra',
+  gender: 'female',
+  age: 25,
+  address: 'Asunción, Paraguay',
+};
+
+const client2 = {
+  name: 'Dori',
+  gender: 'male',
+  age: 27,
+  address: 'Fernando de la mora, Central',
+};
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [TitleHeaderComponent, Card],
+  imports: [TitleHeaderComponent, Card, I18nSelectPipe, I18nPluralPipe],
   templateUrl: './uncommon-page.html',
 })
-export default class UncommonPageComponent {}
+export default class UncommonPageComponent {
+  //i18n Select
+  client = signal(client1);
+  inviteMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+  changeClient() {
+    if (this.client() === client1) {
+      this.client.set(client2);
+      return;
+    }
+    this.client.set(client1);
+  }
+
+  //I18n Plural
+  clients = signal(['Alejandra', 'Dori', 'Ara', 'Lucero', 'Pablo', 'Majo']);
+  clientsMap = signal({
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos 2 clientes esperando',
+    other: 'tenemos # clientes esperando',
+  });
+
+  deleteClient() {
+    this.clients.update((preve) => preve.slice(1));
+  }
+}
